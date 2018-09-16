a3a9dd04637817edb305ca43fba19902ad89437f
Unknown <steven.chou@uniqestate.com>
2018-09-16 21:42:40 +0800

    Modules

---
+++ ts-hello/main.ts
@@ -0,0 +1,6 @@
+import { Point } from './point';
+
+const point = new Point(1, 2);
+point.x = 456;
+console.log(point.x);
+point.draw();
---
+++ ts-hello/point.ts
@@ -0,0 +1,16 @@
+export class Point {
+  constructor(private _x?: number, private _y?: number) {}
+
+  draw() {
+    console.log('>>> X: ' + this._x + ',Y: ' + this._y);
+  }
+
+  set x(value: number) {
+    if (value < 0) throw new Error('value cannot be less than 0.');
+    this._x = value;
+  }
+
+  get X() {
+    return this._x;
+  }
+}
