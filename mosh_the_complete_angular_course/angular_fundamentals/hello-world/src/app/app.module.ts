6223b25e32223928dd0c751f48d7f47a49b7df31
Unknown <steven.chou@uniqestate.com>
2018-09-18 09:26:16 +0800

    first compomemt

--- mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/app.module.ts
+++ mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CoursesComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/courses.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses', // <app-courses> -> css selector
+  template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {}
