13ffdcf8ea70c6426296a52e8d00f7aa31ed3c0e
Unknown <steven.chou@uniqestate.com>
2018-09-18 23:05:01 +0800

    Directives

--- mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/app.module.ts
+++ mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent],
+  declarations: [AppComponent, CoursesComponent, CourseComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
--- mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/courses.component.ts
+++ mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/courses.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses', // <app-courses> -> css selector
-  template: '<h2>Courses</h2>'
+  template: `
+    <h2>{{getTitle()}}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{course}}
+      </li>
+    </ul>
+  `
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  title = 'List of courses';
+  courses = ['course1', 'course2', 'course3'];
+
+  getTitle() {
+    return this.title;
+  }
+}
