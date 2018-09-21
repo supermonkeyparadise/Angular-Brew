122a532fc651987abc7025d877fd31d6c06f96c0
Unknown <steven.chou@uniqestate.com>
2018-09-21 13:36:21 +0800

    authors list

--- mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/app.module.ts
+++ mosh_the_complete_angular_course/angular_fundamentals/hello-world/src/app/app.module.ts
@@ -4,12 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
 import { CoursesService } from './courses.service';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, CourseComponent],
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
+  ],
   imports: [BrowserModule],
-  providers: [CoursesService],
+  providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
